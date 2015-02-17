baecd53a773b60d6ad017ef4a7c1059cb6088e7f
jokullie <jokulle11@ru.is>
2015-02-17 09:32:18 +0000

    jshint

---
+++ Gruntfile.js
@@ -0,0 +1,30 @@
+module.exports = function(grunt){
+	grunt.loadNpmTasks('grunt-contrib-jshint');
+
+	grunt.initConfig({
+		jshint:{
+			src: ['app.js'],
+			gruntfile: ['Gruntfile.js'],
+			options: {
+				curly:  true,
+				immed:  true,
+				newcap: true,
+				noarg:  true,
+				sub:    true,
+				boss:   true,
+				eqnull: true,
+				node:   true,
+				undef:  true,
+				globals: {
+					_:       false,
+					jQuery:  false,
+					angular: false,
+					moment:  false,
+					console: false,
+					$:       false,
+					io:      false
+				}
+			}
+		}
+	});
+};
--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 var app = angular.module("AngChat", []);
 
 angular.module("AngChat").controller("HomeController",
-    function($scope){
-        $scope.message = "Hello World";
+	function($scope){
+		$scope.message = "Hello World";
+
 });
